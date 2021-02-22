0ca07e5895727fcecf88254db5a8d01d4c39c0a5
Madhav Anand <madhav1004@icloud.com>
2021-02-22 20:34:56 +0530

    chat-input is done jsut need to map messsage and voilla.

--- src/Components/ChatBody/ChatInput.tsx
+++ src/Components/ChatBody/ChatInput.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import { Input } from "@material-ui/core";
 import { Picker } from "emoji-mart";
-import { InsertEmoticon } from "@material-ui/icons";
+import { InsertEmoticon, Send } from "@material-ui/icons";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, State } from "../../redux/store";
 import { emojiActions } from "../../redux/emojiSlice";
@@ -16,8 +16,12 @@ const InputContainer = styled.div`
   border-radius: 3px;
   justify-content: space-between;
 
+  * {
+    margin: auto 3px;
+  }
+
   .chat-input {
-    flex: 0.82;
+    flex: 0.96;
     padding: 5px 10px;
   }
 
@@ -28,10 +32,38 @@ const InputContainer = styled.div`
     padding: 4px;
     font-size: 32px;
     font-weight: 300 !important;
+    transition: color 80ms ease-in, background-color 100ms ease-in;
 
     :hover {
       color: black;
       background-color: #f6f6f6;
+    }
+  }
+
+  .send-icon {
+    cursor: pointer;
+    border-radius: 3px;
+    padding: 4px;
+    font-size: 30px;
+    font-weight: 300 !important;
+    transition: color 80ms ease-in, background-color 100ms ease-in;
+    color: white;
+    background-color: #3a8667;
+  }
+`;
+
+const IconButton = styled.button`
+  width: fit-content;
+  height: fit-content;
+  border: none;
+  padding: 0;
+  outline-width: 0;
+  background-color: white;
+
+  :disabled {
+    .send-icon {
+      color: gray;
+      background-color: white;
     }
   }
 `;
@@ -47,6 +79,12 @@ const Emoji = styled.div`
   display: flex;
   flex-direction: column;
   position: relative;
+`;
+
+const Icons = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 function ChatInput(): JSX.Element {
@@ -67,26 +105,32 @@ function ChatInput(): JSX.Element {
           onChange={(e) => setMessage(e.target.value)}
         />
 
-        <Emoji>
-          <Picker
-            sheetSize={32}
-            set={"apple"}
-            onSelect={(emoji) =>
-              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-              // @ts-ignore
-              setMessage((message) => message + emoji.native)
-            }
-            style={{
-              position: "absolute",
-              transform: "translate(-90%, -105%)",
-              visibility: show ? "visible" : "hidden",
-            }}
-          />
-          <InsertEmoticon
-            className={"insert-emoji"}
-            onClick={() => dispatch(emojiActions.SET_SHOW(!show))}
-          />
-        </Emoji>
+        <Icons>
+          <Emoji>
+            <Picker
+              sheetSize={32}
+              set={"apple"}
+              onSelect={(emoji) =>
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-ignore
+                setMessage((message) => message + emoji.native)
+              }
+              style={{
+                position: "absolute",
+                transform: "translate(-90%, -105%)",
+                visibility: show ? "visible" : "hidden",
+              }}
+            />
+            <InsertEmoticon
+              className={"insert-emoji"}
+              onClick={() => dispatch(emojiActions.SET_SHOW(!show))}
+            />
+          </Emoji>
+
+          <IconButton disabled={message.length === 0}>
+            <Send className={"send-icon"} />
+          </IconButton>
+        </Icons>
       </InputContainer>
     </InputWrapper>
   );
